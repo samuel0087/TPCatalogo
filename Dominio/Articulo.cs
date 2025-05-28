cd292b9f1495a15d02976949c6c8227a4a58b995
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2025-05-28 00:13:37 -0300

    Se creo las clases Articulo, Categoria y Marca

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Articulo
+    {
+        private int IdArticulo { set; get; }
+        private string Codigo { set; get; }
+        private string Nombre { set; get; }
+        private string Descripcion { set; get; }
+        private Marca Marca { set; get; }
+        private Categoria Categoria { set; get; }
+        private string ImagenUrl { set; get; }
+        private decimal Precio { set; get; }
+    }
+}
---
+++ Dominio/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    internal class Marca
+    {
+        private int IdMarca { set; get; }
+        private string Descripcion { set; get; }
+    }
+}
