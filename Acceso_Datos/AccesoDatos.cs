c8d87c92eaf51f1d7e7b5f0e0cbd7e4d261f6b3b
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2025-05-28 01:01:10 -0300

    Se creo la clase Acceso a datos

---
+++ Acceso_Datos/AccesoDatos.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Acceso_Datos
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        private SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        //Se crea la conexion a la base de datos en el constructor
+        public AccesoDatos()
+        {
+            const string DB = "CATALOGO_DB"; //Nombre de la base de datos
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=" + DB + "; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        //Recibe la consulta
+        public void setearConsulta(string query)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = query;
+        }
+
+        //Setea el parametro deseado
+        public void setearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        //Ejecuta la consulta Sql y guarda el resultado en el lector
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+        }
+
+        public void ejecitarAccion()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally 
+            { 
+                cerrarConexion(); 
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            try
+            {
+                if (lector != null && !lector.IsClosed)
+                {
+                    lector.Close();
+                }
+                if (conexion != null && conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar excepciones, si es necesario
+                throw new Exception("Error al cerrar la conexión: " + ex.Message);
+            }
+        }
+    }
+}
